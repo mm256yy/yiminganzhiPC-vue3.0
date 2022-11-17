b3abecbb410d6d4d5aa86a9e934bceba5900eae7
胡军伟 <maxwell_hu@foxmail.com>
2022-11-17 15:48:44 +0800

    feat: 添加项目管理页面模板

--- src/views/Project/Admin/Index.vue
+++ src/views/Project/Admin/Index.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>项目管理</div>
+  <ContentWrap title="项目管理">
+    <div>&nbsp;</div>
+  </ContentWrap>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { ContentWrap } from '@/components/ContentWrap'
+
+onMounted(() => {
+  // mounted
+})
+</script>
