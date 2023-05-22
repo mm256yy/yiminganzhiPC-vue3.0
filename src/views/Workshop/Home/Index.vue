fa9fbc9aefdde9e2884224a4e1b8659f2beecfd2
446858845@qq.com <446858845@qq.com>
2023-05-22 18:00:37 +0800

    fix: 加参数

--- src/views/Workshop/Home/Index.vue
+++ src/views/Workshop/Home/Index.vue
@@ -958,9 +958,6 @@ onMounted(async () => {
 
   data2 = await getTopTen('today')
 
-  seriesdata.value = []
-  ydataName.value = []
-  seriesdata2.value = []
   data.forEach((item: any, index: number) => {
     if (index <= 4) {
       ydataName.value.push(item.name)
