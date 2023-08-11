5a3b4d87a06f902fce39597a9d95aada4d5f6db9
oldKing <zhangwx@erongdu.com>
2023-08-11 16:31:01 +0800

    feat: 新增页面提交

--- src/views/Workshop/ImmigrantImplement/IndividualB/Index.vue
+++ src/views/Workshop/ImmigrantImplement/IndividualB/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <WorkContentWrap>
     <ElBreadcrumb separator="/">
-      <ElBreadcrumbItem class="text-size-12px">信息填报</ElBreadcrumbItem>
-      <ElBreadcrumbItem class="text-size-12px">个体工商信息{{ titleStatus }}</ElBreadcrumbItem>
+      <ElBreadcrumbItem class="text-size-12px">移民实施</ElBreadcrumbItem>
+      <ElBreadcrumbItem class="text-size-12px">个体工商信息</ElBreadcrumbItem>
     </ElBreadcrumb>
     <div class="search-form-wrap">
       <Search
@@ -19,23 +19,13 @@
       <div class="flex items-center justify-between pb-12px">
         <div class="table-header-left">
           <span style="margin: 0 10px; font-size: 16px; font-weight: 600">个体工商列表</span>
-
           <div class="icon">
             <Icon icon="heroicons-outline:light-bulb" color="#fff" :size="18" />
           </div>
           <div class="text">
             共 <span class="num">{{ headInfo.peasantHouseholdNum }}</span> 家个体工商
-            <span class="distance"></span>
-            已填报<span class="num !text-[#30A952]">{{ headInfo.reportSucceedNum }}</span> 家
-            <span class="distance"></span>
-            未填报<span class="num !text-[#FF3030]">{{ headInfo.unReportNum }}</span> 家
           </div>
         </div>
-        <ElSpace>
-          <ElButton type="primary" @click="onExport">数据导出</ElButton>
-          <ElButton :icon="addIcon" type="primary" @click="onAddRow">新增个体工商</ElButton>
-          <ElButton :icon="printIcon" type="default" @click="onPrint">打印表格</ElButton>
-        </ElSpace>
       </div>
       <Table
         selection
@@ -72,17 +62,6 @@
         <template #hasPropertyAccount="{ row }">
           <div>{{ row.hasPropertyAccount ? '是' : '否' }}</div>
         </template>
-        <template #fillStatus="{ row }">
-          <div class="flex items-center justify-center">
-            <span
-              :class="[
-                'status',
-                row.fillStatus === ReportStatus.ReportSucceed ? 'status-suc' : 'status-err'
-              ]"
-            ></span>
-            {{ row.fillStatus === ReportStatus.ReportSucceed ? '已填报' : '未填报' }}</div
-          >
-        </template>
         <template #reportDate="{ row }">
           <div>{{ formatDate(row.reportDate) }}</div>
         </template>
@@ -90,174 +69,61 @@
           <div class="filling-btn" @click="fillData(row)">数据填报</div>
         </template>
         <template #action="{ row }">
-          <TableEditColumn
-            :view-type="'link'"
-            :icons="[
-              {
-                icon: '',
-                tooltip: '查看',
-                type: 'primary',
-                action: () => onViewRow(row)
-              }
-            ]"
-            :row="row"
-            @edit="onEditRow(row)"
-            @delete="onDelRow"
-          />
+          <ElButton type="primary" link @click="onEditRow(row)">编辑</ElButton>
         </template>
       </Table>
     </div>
 
     <EditForm
       :show="dialog"
-      :actionType="actionType"
       :row="tableObject.currentRow"
       @close="onFormPupClose"
       @update-district="onUpdateDistrict"
     />
-
-    <Print
-      :show="printDialog"
-      :landlordIds="landlordIds"
-      :templateType="PrintType.printIndividualHousehold"
-      @close="onPrintDialogClose"
-      :outsideData="outsideData"
-    />
-    <Export
-      :show="exportDialog"
-      :type="'IndividualHousehold'"
-      :list="exportList"
-      @close="onExportDialogClose"
-    />
-    <Survey :show="surveyDialog" :data="surveyInfo" @close="onSurveyDialogClose" />
   </WorkContentWrap>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { globalData } from '@/config/fill'
-import { SurveyStatusEnum } from '@/views/Workshop/components/config'
-
-export default defineComponent({
-  beforeRouteEnter(to, _from, next) {
-    console.log(to, 'to')
-    if (to.path === '/Workshop/IndividualB') {
-      // 实物采集
-      globalData.currentSurveyStatus = SurveyStatusEnum.Survey
-    } else {
-      // 实物复核
-      globalData.currentSurveyStatus = SurveyStatusEnum.Review
-    }
-    next()
-  }
-})
-</script>
 
 <script setup lang="ts">
 import { reactive, ref, onMounted } from 'vue'
 import { useAppStore } from '@/store/modules/app'
-import {
-  ElButton,
-  ElSpace,
-  ElBreadcrumb,
-  ElBreadcrumbItem,
-  ElMessageBox,
-  ElMessage
-} from 'element-plus'
+import { ElButton, ElBreadcrumb, ElBreadcrumbItem } from 'element-plus'
 import { WorkContentWrap } from '@/components/ContentWrap'
 import { Search } from '@/components/Search'
-import { Table, TableEditColumn } from '@/components/Table'
-import EditForm from './components/EditForm.vue'
-import Print from '../components/Print.vue'
-import Export from '../components/Export.vue'
-import Survey from './components/Survey.vue'
+import { Table } from '@/components/Table'
+import EditForm from './EditForm.vue'
 import { CrudSchema, useCrudSchemas } from '@/hooks/web/useCrudSchemas'
 import { useTable } from '@/hooks/web/useTable'
-import { useIcon } from '@/hooks/web/useIcon'
-import {
-  getLandlordListApi,
-  delLandlordByIdApi,
-  getLandlordHeadApi,
-  getLandlordSurveyByIdApi
-} from '@/api/workshop/landlord/service'
+import { getLandlordListApi, getLandlordHeadApi } from '@/api/immigrantImplement/common-service'
 import { screeningTree } from '@/api/workshop/village/service'
-import { locationTypes } from '@/views/Workshop/components/config'
-import { ReportStatus } from '@/views/Workshop/DataFill/config'
+import { locationTypes } from '../DataFill/config'
 import { useRouter } from 'vue-router'
-import type {
-  LandlordDtoType,
-  LandlordHeadInfoType,
-  SurveyInfoType
-} from '@/api/workshop/landlord/types'
+import type { LandlordDtoType, LandlordHeadInfoType } from '@/api/workshop/landlord/types'
 import { formatDate } from '@/utils/index'
-import { PrintType } from '@/types/print'
 
-const router = useRouter()
-const titleStatus = router.currentRoute.value?.meta?.title?.split('-')[1]
-  ? router.currentRoute.value?.meta?.title?.split('-')[1]
-  : '采集'
 const appStore = useAppStore()
 const { push } = useRouter()
 const projectId = appStore.currentProjectId
 const dialog = ref(false) // 弹窗标识
-const actionType = ref<'add' | 'edit' | 'view'>('add') // 操作类型
-const addIcon = useIcon({ icon: 'ant-design:plus-outlined' })
-const printIcon = useIcon({ icon: 'ion:print-outline' })
 const villageTree = ref<any[]>([])
-const landlordIds = ref<number[]>([])
+
 const headInfo = ref<LandlordHeadInfoType>({
   demographicNum: 0,
   peasantHouseholdNum: 0,
   reportSucceedNum: 0,
   unReportNum: 0
 })
-const printDialog = ref(false)
-const exportDialog = ref(false)
-interface exportListType {
-  name: string
-  value: string | number
-}
-const exportList = ref<exportListType[]>([
-  {
-    name: '个体工商户统计表',
-    value: 'exportCompanyRegister'
-  },
-  {
-    name: '个体工商基本情况统计表',
-    value: 'exportCompanyInfo'
-  },
-  {
-    name: '个体工商房屋信息统计表',
-    value: 'exportCompanyHouse'
-  },
-  {
-    name: '个体工商附属物调查统计表',
-    value: 'exportCompanyAppendage'
-  },
-  {
-    name: '个体工商零星林果木调查统计表',
-    value: 'exportCompanyTree'
-  },
-  {
-    name: '个体工商设施设备调查统计表',
-    value: 'exportCompanyEquipment'
-  }
-])
-const surveyDialog = ref(false)
-const surveyInfo = ref<SurveyInfoType | null>(null)
-const outsideData = ref<any>([])
+
 const { register, tableObject, methods } = useTable({
-  getListApi: getLandlordListApi,
-  delListApi: delLandlordByIdApi
+  getListApi: getLandlordListApi
 })
 
-const { setSearchParams, getSelections } = methods
+const { setSearchParams } = methods
 
 tableObject.params = {
   projectId
 }
 
-setSearchParams({ type: 'IndividualHousehold' })
+setSearchParams({ type: 'IndividualHousehold', status: 'implementation' })
 
 const getVillageTree = async () => {
   const list = await screeningTree(projectId, 'IndividualHousehold')
@@ -407,8 +273,8 @@ const schema = reactive<CrudSchema[]>([
     }
   },
   {
-    field: 'fillStatus',
-    label: '填报状态',
+    field: 'schedule',
+    label: '完成状态',
     search: {
       show: false
     }
@@ -455,46 +321,7 @@ const schema = reactive<CrudSchema[]>([
 
 const { allSchemas } = useCrudSchemas(schema)
 
-const onDelRow = async (row: LandlordDtoType) => {
-  tableObject.currentRow = row
-  const result = await getLandlordSurveyByIdApi(row?.id)
-  ElMessageBox.confirm(
-    `
-    <div style='text-align:center'>
-        <strong>${row.name}个体工商包含:</strong>
-
-      <div>房屋信息: ${result.immigrantHouseList.length} 栋房屋信息</div>
-      <div>附属物信息: ${result.immigrantAppendantList.length} 项附属物信息</div>
-      <div>零星(林)果木信息: ${result.immigrantTreeList.length} 项零星果木信息</div>
-
-      <strong>是否删除该个体工商信息</strong>
-    </div>
-  `,
-    '提示',
-    {
-      dangerouslyUseHTMLString: true,
-
-      cancelButtonText: '取消',
-      confirmButtonText: '确认'
-    }
-  )
-    .then(() => {
-      delLandlordByIdApi(tableObject.currentRow?.id as number).then(() => {
-        setSearchParams({ type: 'IndividualHousehold' })
-        getLandlordHeadInfo()
-      })
-    })
-    .catch(() => {})
-}
-
-const onAddRow = () => {
-  actionType.value = 'add'
-  tableObject.currentRow = null
-  dialog.value = true
-}
-
 const onEditRow = (row: LandlordDtoType) => {
-  actionType.value = 'edit'
   tableObject.currentRow = row
   dialog.value = true
 }
@@ -502,7 +329,7 @@ const onEditRow = (row: LandlordDtoType) => {
 const onFormPupClose = (flag: boolean) => {
   dialog.value = false
   if (flag === true) {
-    setSearchParams({ type: 'IndividualHousehold' })
+    setSearchParams({ type: 'IndividualHousehold', status: 'implementation' })
     getLandlordHeadInfo()
   }
 }
@@ -538,8 +365,8 @@ const onSearch = (data) => {
   let params = {
     ...data
   }
-  if (!data.reportStatus) {
-    Reflect.deleteProperty(params, 'reportStatus')
+  if (!data.fillStatus) {
+    Reflect.deleteProperty(params, 'fillStatus')
   }
 
   // 需要重置一次params
@@ -558,61 +385,24 @@ const onSearch = (data) => {
       }
 
       params.type = 'IndividualHousehold'
-      setSearchParams({ ...params })
+      setSearchParams({ ...params, status: 'implementation' })
     })
   } else {
     params.type = 'IndividualHousehold'
-    setSearchParams({ ...params })
+    setSearchParams({ ...params, status: 'implementation' })
   }
-}
-
-const onPrint = async () => {
-  const res = await getSelections()
-  if (res && res.length) {
-    landlordIds.value = res.map((item) => item.id)
-    printDialog.value = true
-    outsideData.value = res.map((item) => item.name)
-  } else {
-    ElMessage.warning('请选择需要打印的个体工商')
-  }
-}
-
-const onPrintDialogClose = () => {
-  printDialog.value = false
-}
-
-const onExport = () => {
-  exportDialog.value = true
-}
-
-const onExportDialogClose = () => {
-  exportDialog.value = false
 }
 
 // 数据填报
 const fillData = (row) => {
-  let routerName = 'DataFill'
-  if (globalData.currentSurveyStatus === SurveyStatusEnum.Review) {
-    routerName = 'DataFillCheck'
-  }
   push({
-    name: routerName,
+    name: 'immigrantImpDataFill',
     query: {
       householdId: row.id,
       doorNo: row.doorNo,
       type: 'IndividualB'
     }
   })
-}
-
-const onSurveyDialogClose = () => {
-  surveyDialog.value = false
-}
-
-const onViewRow = async (row) => {
-  actionType.value = 'view'
-  tableObject.currentRow = row
-  dialog.value = true
 }
 </script>
 
