04da4e2bfb5cbfed558fd057a5173e52564238a9
2497976487@qq.com <2497976487@qq.com>
2023-09-08 11:28:29 +0800

    feat: 新建路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -302,6 +302,15 @@ export const workshopConstantRouterMap: AppRouteRecordRaw[] = [
           title: '乡镇资金入账',
           hidden: true
         }
+      },
+      {
+        path: 'PaymentApplication',
+        name: 'PaymentApplication',
+        component: () => import('@/views/Workshop/FundManage/PaymentApplication/Index.vue'),
+        meta: {
+          title: '付款申请',
+          hidden: false
+        }
       }
     ]
   }
---
+++ src/views/Workshop/FundManage/PaymentApplication/Index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>11111111111 </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
