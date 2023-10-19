6017d6fdb8f2f6a249536a52f6c67029e2082ea4
zhangmh@erongdu.com <zhangmh@erongdu.com>
2023-10-19 15:01:01 +0800

    feat: 资金调试

--- src/views/Workshop/FundManage/FundPayment/EditForm.vue
+++ src/views/Workshop/FundManage/FundPayment/EditForm.vue
@@ -16,56 +16,49 @@
       :label-position="'right'"
       :rules="rules"
     >
-      <ElFormItem label="申请类型:" required>
-        <el-radio-group class="ml-4" v-model="form.applyType">
-          <el-radio
-            v-for="item in dictObj[381]"
-            :label="item.value"
-            size="large"
-            :key="item.value"
-            >{{ item.label }}</el-radio
-          >
-        </el-radio-group>
+      <ElFormItem label="申请类型:" required prop="applyType">
+        <ElRadioGroup class="ml-4" v-model="form.applyType">
+          <ElRadio v-for="item in dictObj[381]" :label="item.value" :key="item.value">{{
+            item.label
+          }}</ElRadio>
+        </ElRadioGroup>
       </ElFormItem>
-      <ElFormItem label="申请名称:" required>
+      <ElFormItem label="申请名称:" required prop="name">
         <ElInput type="text" v-model="form.name" />
       </ElFormItem>
-      <ElFormItem label="概算科目:" required>
-        <el-radio-group class="ml-4" v-model="form.type">
-          <el-radio
-            v-for="item in dictObj[382]"
-            :label="item.value"
-            size="large"
-            :key="item.value"
-            >{{ item.label }}</el-radio
-          >
-        </el-radio-group>
+      <ElFormItem label="概算科目:" required prop="type">
+        <ElRadioGroup class="ml-4" v-model="form.type">
+          <ElRadio v-for="item in dictObj[382]" :label="item.value" :key="item.value">{{
+            item.label
+          }}</ElRadio>
+        </ElRadioGroup>
       </ElFormItem>
 
-      <ElFormItem label="资金科目:" required>
+      <ElFormItem label="资金科目:" required prop="funSubjectId">
         <ElTreeSelect
           class="!w-full"
           v-model="form.funSubjectId"
-          :data="fundAccountList"
-          node-key="code"
+          :data="props.fundAccountList"
           :props="{ value: 'code', label: 'name' }"
+          node-key="code"
           showCheckbox
           checkStrictly
           checkOnClickNode
+          :default-checked-keys="[form.funSubjectId]"
         />
       </ElFormItem>
 
-      <ElFormItem label="付款说明:" required>
+      <ElFormItem label="付款说明:" required prop="remark">
         <ElInput type="text" v-model="form.remark" />
       </ElFormItem>
 
-      <ElFormItem label="收款单位:" required>
+      <ElFormItem label="收款单位:" required prop="receivePaymentUnit">
         <ElInput type="text" v-model="form.receivePaymentUnit" />
       </ElFormItem>
-      <ElFormItem label="付款时间:" required>
+      <ElFormItem label="付款时间:" required prop="paymentTime">
         <ElDatePicker type="date" v-model="form.paymentTime" />
       </ElFormItem>
-      <ElFormItem label="申请金额:">
+      <ElFormItem label="申请金额:" prop="amount">
         <ElInputNumber type="text" v-model="form.amount" />
       </ElFormItem>
       <div class="col-wrapper">
@@ -132,19 +125,19 @@ import {
   ElTreeSelect,
   ElInputNumber
 } from 'element-plus'
-import { ref, reactive, nextTick, watch, computed, onMounted } from 'vue'
+import { ref, reactive, nextTick, watch, computed } from 'vue'
 import { debounce } from 'lodash-es'
 import type { UploadFile, UploadFiles } from 'element-plus'
 import { useAppStore } from '@/store/modules/app'
 import { addFunPayApi, updateFunPayApi } from '@/api/fundManage/fundPayment-service'
 import { useDictStoreWithOut } from '@/store/modules/dict'
 import dayjs from 'dayjs'
-import { getFundSubjectListApi } from '@/api/fundManage/common-service'
 
 interface PropsType {
   show: boolean
   actionType: 'add' | 'edit' | 'view'
   row?: any
+  fundAccountList: any[]
 }
 
 interface FileItemType {
@@ -158,8 +151,8 @@ const formRef = ref<FormInstance>()
 const appStore = useAppStore()
 const dictStore = useDictStoreWithOut()
 const dictObj = computed(() => dictStore.getDictObj)
-const fundAccountList = ref<any[]>([]) // 资金科目
 
+console.log(dictObj.value[382], '382')
 const form = ref<any>({})
 const imgUrl = ref<string>('')
 const dialogVisible = ref<boolean>(false)
@@ -174,23 +167,28 @@ const headers = {
 const rules = reactive<FormRules>({})
 
 watch(
-  () => props.show,
+  () => props.row,
   (val) => {
     if (val) {
-      if (props.actionType === 'edit') {
-        // 编辑
-        form.value = {
-          ...props.row
-        }
-        receipt.value = JSON.parse(props.row.receipt)
-        if (form.value.paymentTime) {
-          form.value.paymentTime = dayjs(form.value.paymentTime).format('YYYY-MM-DD')
-        }
-      } else {
-        // 新增
-        form.value = {}
+      console.log(val, '当前对象')
+      // 编辑
+      form.value = {
+        ...val
       }
+      if (val.receipt) {
+        receipt.value = JSON.parse(val.receipt)
+      }
+      if (form.value.paymentTime) {
+        form.value.paymentTime = dayjs(form.value.paymentTime).format('YYYY-MM-DD')
+      }
+    } else {
+      // 新增
+      form.value = {}
     }
+  },
+  {
+    immediate: true,
+    deep: true
   }
 )
 
@@ -225,6 +223,7 @@ const submit = (data: any, status?: number) => {
 const onSubmit = debounce((formEl, status?: number) => {
   formEl?.validate((valid: any) => {
     if (valid) {
+      console.log(form.value, 'form')
       if (!receipt.value.length) {
         ElMessage.error('请上传搬迁安置确认单')
         return
@@ -288,19 +287,6 @@ const imgPreview = (uploadFile: UploadFile) => {
 const onError = () => {
   ElMessage.error('上传失败,请上传5M以内的图片或者重新上传')
 }
-
-// 获取资金科目选项列表
-const getFundSubjectList = () => {
-  getFundSubjectListApi().then((res: any) => {
-    if (res) {
-      fundAccountList.value = res.content
-    }
-  })
-}
-
-onMounted(() => {
-  getFundSubjectList()
-})
 </script>
 
 <style lang="less" scoped>
