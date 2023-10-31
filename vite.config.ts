681972a965e48ba740072df0d7c61732af3518b8
zhangmh@erongdu.com <zhangmh@erongdu.com>
2023-10-31 17:47:41 +0800

    feat: 本地运行改动

--- vite.config.ts
+++ vite.config.ts
@@ -128,7 +128,7 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
       }),
       VueMarcos(),
       createHtmlPlugin({
-        pages: env.VITE_USER_NODE_ENV === 'development' ?
+        pages: mode === 'dev' ?
         [
          ...pcHmtlEntrys,
          ...h5HmtlEntrys
@@ -174,7 +174,14 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
         }
       },
       rollupOptions: {
-        input: platform === 'pc' ? {
+        input: 
+        mode === 'dev' ? {
+          main: resolve(__dirname, "index.html"),
+          admin: resolve(__dirname, "admin.html"),
+          h5: resolve(__dirname, "h5.html"),
+          ld: resolve(__dirname, "ld.html")
+        } :
+        platform === 'pc' ? {
           main: resolve(__dirname, "index.html"),
           admin: resolve(__dirname, "admin.html"),
         } :
