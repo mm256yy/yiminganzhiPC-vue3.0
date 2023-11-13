af3102072c381cee44ada5cbbfafd40ae01ea2c5
1019314017@qq.com <1019314017@qq.com>
2023-11-13 16:50:25 +0800

    fix: 评估bug

--- src/views/Workshop/AssetEvaluation/DataFill/components/LandGreenSeedlings/Index.vue
+++ src/views/Workshop/AssetEvaluation/DataFill/components/LandGreenSeedlings/Index.vue
@@ -288,7 +288,8 @@ const onSave = () => {
 
 // 自动计算评估金额
 const getModelValue = (row: any) => {
-  const totalPrice = Number(row.number) * Number(row.price) * Number(row.rate)
+  const totalPrice =
+    Number(row.number) * Number(row.price) * (Number(row.rate) == 0 ? 1 : Number(row.rate))
   return totalPrice
 }
 
