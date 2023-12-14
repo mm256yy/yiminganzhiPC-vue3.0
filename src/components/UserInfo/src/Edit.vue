625c7af496bbae001ff9f46d866a9c3b596f5c4d
fcx <wy15869011453@163.com>
2023-12-14 18:43:58 +0800

    feat: 修改密码

--- src/components/UserInfo/src/Edit.vue
+++ src/components/UserInfo/src/Edit.vue
@@ -16,62 +16,84 @@
       :label-position="'right'"
       :rules="rules"
     >
-      <ElFormItem label="旧密码:" required prop="remark">
-        <ElInput type="password" v-model="form.remark" />
+      <ElFormItem label="旧密码:" required prop="oldPass">
+        <ElInput type="password" v-model="form.oldPass" />
       </ElFormItem>
-      <ElFormItem label="新密码:" required prop="remark">
-        <ElInput type="password" v-model="form.remark" />
+      <ElFormItem label="新密码:" required prop="newPass">
+        <ElInput type="password" v-model="form.newPass" />
       </ElFormItem>
-      <ElFormItem label="确认密码:" required prop="remark">
-        <ElInput type="password" v-model="form.remark" />
+      <ElFormItem label="确认新密码:" required prop="confirmNewPass">
+        <ElInput type="password" v-model="form.confirmNewPass" />
       </ElFormItem>
     </ElForm>
     <template #footer>
       <ElButton @click="onClose">取消</ElButton>
-      <ElButton type="primary" @click="onSubmit(formRef, 1)">确认修改</ElButton>
+      <ElButton type="primary" @click="onSubmit(formRef)">确认修改</ElButton>
     </template>
   </ElDialog>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { ElDialog, ElButton, ElFormItem, ElInput, ElForm, ElMessage } from 'element-plus'
+import { ref, reactive, nextTick } from 'vue'
+import {
+  ElDialog,
+  ElButton,
+  ElFormItem,
+  ElInput,
+  ElForm,
+  ElMessage,
+  FormRules,
+  FormInstance
+} from 'element-plus'
 import { debounce } from 'lodash-es'
+import { useValidator } from '@/hooks/web/useValidator'
+import { modifyPasswordApi } from '@/api/login'
+import { useAppStore } from '@/store/modules/app'
+
+const { required } = useValidator()
+const appStore = useAppStore()
 const form = ref<any>({})
 interface PropsType {
   show: any
 }
 const props = defineProps<PropsType>()
 const emit = defineEmits(['close', 'updateDistrict'])
+const formRef = ref<FormInstance>()
+// 规则校验
+const rules = reactive<FormRules>({
+  oldPass: [required()],
+  newPass: [required()],
+  confirmNewPass: [required()]
+})
 
 // 关闭弹窗
 const onClose = (flag = false) => {
   emit('close', flag)
-  //   nextTick(() => {
-  //     formRef.value?.resetFields()
-  //   })
+  nextTick(() => {
+    formRef.value?.resetFields()
+  })
 }
 
 const submit = async (data: any) => {
-  //   if (props.actionType === 'add') {
-  //     data.projectId = appStore.getCurrentProjectId
-  //     await addFunPayApi(data)
-  //   } else {
-  //     await updateFunPayApi(data)
-  //   }
+  const result = await modifyPasswordApi(data)
+  console.log('POL-User', result)
+
   ElMessage.success('修改成功！')
-  emit('close', flag)
+  emit('close', true)
 }
 
 // 提交表单
-const onSubmit = debounce((formEl, status: number) => {
+const onSubmit = debounce((formEl) => {
   if (!formEl) return
-  formEl.validate((valid, fields) => {
+  formEl.validate((valid) => {
     if (valid) {
+      const userInfo = appStore.getUserInfo
+
       let params: any = {
-        ...form.value
+        userName: userInfo?.userName,
+        oldPass: form.value.oldPass,
+        newPass: form.value.newPass
       }
-      params.status = status
       submit(params)
     }
   })
