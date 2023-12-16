68125debcd5e9ddcd569104ac725f9413a7286d9
2497976487@qq.com <2497976487@qq.com>
2023-12-16 11:22:06 +0800

    feat: init

--- src/views/Workshop/ExternalLink/components/left.vue
+++ src/views/Workshop/ExternalLink/components/left.vue
@@ -40,14 +40,156 @@
         </template>
       </Label>
 
-      <view class="contain_ym_box" />
+      <div class="contain_ym_box">
+        <div class="tip_box">
+          <img class="icon" src="../../../../assets/imgs/homes/Polygon.png" alt="" />
+          <div class="left_text">永久用地</div>
+          <div class="right_tip_box">
+            共
+            <span>14,375.6 </span>
+            亩
+          </div>
+        </div>
+        <div class="permanent">
+          <div class="permanent_box">
+            <div class="permanent_box_top">水库淹没区</div>
+            <div class="permanent_box_bom">13643.97<span> 亩</span></div>
+          </div>
+          <div class="permanent_box">
+            <div class="permanent_box_top">枢纽工程建设区</div>
+            <div class="permanent_box_bom">13643.97<span> 亩</span></div>
+          </div>
+          <div class="permanent_box">
+            <div class="permanent_box_top">输水工程建设区</div>
+            <div class="permanent_box_bom">13643.97<span> 亩</span></div>
+          </div>
+        </div>
+        <div class="tip_box people_tip">
+          <img class="icon" src="../../../../assets/imgs/homes/Polygon.png" alt="" />
+          <div class="left_text">人口</div>
+        </div>
+        <div class="people_info">
+          <div class="people_box">
+            <div>
+              <img class="prople_img" src="../../../../assets/imgs/homes/ico_left.png" alt="" />
+            </div>
+            <div class="people_con">
+              <span class="people_text">总户数</span>
+              <span class="num">4,961 </span>
+              <span>户</span>
+            </div>
+            <div class="people_right">
+              <img class="right_icon" src="../../../../assets/imgs/homes/Polygons.png" alt="" />
+              <span>财产户</span>
+              144户
+            </div>
+          </div>
+        </div>
+        <div class="people_info">
+          <div class="people_box">
+            <div>
+              <img class="prople_img" src="../../../../assets/imgs/homes/ico_bom.png" alt="" />
+            </div>
+            <div class="people_con">
+              <span class="people_text">总人数</span>
+              <span class="num">4,961 </span>
+              <span>人</span>s
+            </div>
+            <div class="people_right">
+              <img class="right_icon" src="../../../../assets/imgs/homes/Polygons.png" alt="" />
+              <span>农业</span>
+              144人
+              <span class="people_border"></span>
+              <span>非农</span>
+              144人
+            </div>
+          </div>
+        </div>
+        <div class="tip_box">
+          <img class="icon" src="../../../../assets/imgs/homes/Polygon.png" alt="" />
+          <div class="left_text">房屋</div>
+        </div>
+        <div class="permanent">
+          <div class="permanent_box">
+            <div class="permanent_box_top">总房屋面积</div>
+            <div class="permanent_box_bom">13643.97<span> ㎡</span></div>
+          </div>
+          <div class="permanent_box">
+            <div class="permanent_box_top">住宅房屋</div>
+            <div class="permanent_box_bom">13643.97<span> ㎡</span></div>
+          </div>
+          <div class="permanent_box">
+            <div class="permanent_box_top">村集体房屋</div>
+            <div class="permanent_box_bom">13643.97<span> ㎡</span></div>
+          </div>
+        </div>
+        <div class="tip_box">
+          <img class="icon" src="../../../../assets/imgs/homes/Polygon.png" alt="" />
+          <div class="left_text">企（事）业单位</div>
+        </div>
+        <div class="business">
+          <div class="business_li">
+            <div class="business_li_l">
+              <img class="ico_a" src="../../../../assets/imgs/homes/ico_a.png" alt="" />
+            </div>
+            <div class="business_li_r">
+              <div>企业</div>
+              <div class="bom_text">476 <span>家</span></div>
+            </div>
+          </div>
+          <div class="business_li">
+            <div class="business_li_l">
+              <img class="ico_a" src="../../../../assets/imgs/homes/ico_a.png" alt="" />
+            </div>
+            <div class="business_li_r">
+              <div>企业</div>
+              <div class="bom_text">476 <span>家</span></div>
+            </div>
+          </div>
+          <div class="business_li">
+            <div class="business_li_l">
+              <img class="ico_a" src="../../../../assets/imgs/homes/ico_a.png" alt="" />
+            </div>
+            <div class="business_li_r">
+              <div>企业</div>
+              <div class="bom_text">476 <span>家</span></div>
+            </div>
+          </div>
+          <div class="business_li">
+            <div class="business_li_l">
+              <img class="ico_a" src="../../../../assets/imgs/homes/ico_a.png" alt="" />
+            </div>
+            <div class="business_li_r">
+              <div>企业</div>
+              <div class="bom_text">476 <span>家</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="tip_box">
+          <img class="icon" src="../../../../assets/imgs/homes/Polygon.png" alt="" />
+          <div class="left_text">主要专业项目</div>
+        </div>
+        <div class="project">
+          <div class="project_table">
+            <div class="table_li">
+              <div class="table_name"><span class="table_cur"></span> 四级公路</div>
+              <div class="tabel_value">32.24 km</div>
+            </div>
+          </div>
+          <div class="project_table_r">
+            <div class="table_li">
+              <div class="table_name"><span class="table_cur"></span> 四级公路</div>
+              <div class="tabel_value">32.24 km</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import Label from './label.vue'
-
 import { ref, onMounted } from 'vue'
+import Label from './label.vue'
 import { getVillageList } from '@/api/AssetEvaluation/leader-side'
 import { ElTabs, ElTabPane, ElSelect, ElOption } from 'element-plus'
 
@@ -109,6 +251,242 @@ const clearInput = () => {}
     opacity: 1;
     border: 2px solid rgba(62, 115, 236, 0.7);
     .contain_ym_box {
+      padding: 16px;
+      .tip_box {
+        display: flex;
+
+        .icon {
+          width: 7px;
+          height: 9px;
+          margin-right: 8px;
+          margin-top: 2px;
+        }
+        .left_text {
+          font-size: 14px;
+          font-family: PingFang SC, PingFang SC;
+          font-weight: 500;
+          color: #131313;
+          line-height: 16px;
+          margin-right: 8px;
+        }
+        .right_tip_box {
+          padding: 2px 10px;
+          background: #f3f7ff;
+          border-radius: 4px 4px 4px 4px;
+          font-size: 14px;
+          font-family: PingFang SC, PingFang SC;
+          font-weight: 400;
+          color: #333333;
+          line-height: 16px;
+          margin-top: -2px;
+          span {
+            font-weight: 600;
+            color: #3e73ec;
+            margin-right: 5px;
+          }
+        }
+      }
+      .people_tip {
+        margin-bottom: 10px;
+      }
+      .permanent {
+        width: 100%;
+        height: 72px;
+        border-radius: 4px 4px 4px 4px;
+        opacity: 1;
+        border: 1px solid rgba(62, 115, 236, 0.2);
+        margin-top: 10px;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 18px;
+
+        .permanent_box {
+          font-size: 14px;
+          font-family: PingFang SC, PingFang SC;
+          font-weight: 500;
+          color: #666666;
+          line-height: 20px;
+          text-align: center;
+          flex: 1;
+        }
+        .permanent_box_top {
+          margin-top: 10px;
+          margin-bottom: 8px;
+        }
+        .permanent_box_bom {
+          font-size: 20px;
+          font-family: DIN Medium, DIN Medium;
+          font-weight: 400;
+          color: #171718;
+          line-height: 23px;
+          span {
+            font-size: 14px;
+            font-weight: none;
+          }
+        }
+      }
+      .people_info {
+        padding-left: 7px;
+        .people_box {
+          display: flex;
+          .prople_img {
+            width: 56px;
+            height: 56px;
+          }
+          .people_con {
+            width: 178px;
+            height: 44px;
+            border-radius: 4px 4px 4px 4px;
+            opacity: 1;
+            border: 1px solid rgba(62, 115, 236, 0.2);
+            margin-left: 8px;
+            line-height: 40px;
+            text-align: center;
+            font-size: 14px;
+            .num {
+              font-size: 20px;
+              font-family: DIN Medium, DIN Medium;
+              font-weight: 400;
+              color: #171718;
+            }
+            .people_text {
+              margin-right: 8px;
+              font-size: 14px;
+              font-family: PingFang SC, PingFang SC;
+              font-weight: 500;
+              color: #666666;
+            }
+          }
+          .people_right {
+            width: 324px;
+            height: 44px;
+            background: #f3f7ff;
+            border-radius: 4px 4px 4px 4px;
+            opacity: 1;
+            margin-left: 16px;
+            position: relative;
+            text-align: center;
+            line-height: 44px;
+            font-size: 14px;
+            font-family: PingFang SC, PingFang SC;
+            font-weight: 400;
+            color: #000000;
+
+            .people_border {
+              width: 0px;
+              height: 14px;
+              opacity: 1;
+              border: 1px solid #dddddd;
+              display: inline-block;
+              margin: 0 16px;
+            }
+            span {
+              font-size: 14px;
+              font-family: PingFang SC, PingFang SC;
+              font-weight: 500;
+              color: #666666;
+              margin-right: 8px;
+            }
+            .right_icon {
+              position: absolute;
+              left: -9px;
+              top: 16px;
+              width: 9px;
+              height: 12px;
+            }
+          }
+        }
+      }
+      .business {
+        display: flex;
+        margin-top: 11px;
+        padding-left: 13px;
+        margin-bottom: 18px;
+        .business_li {
+          display: flex;
+          flex: 1;
+          .ico_a {
+            width: 40px;
+            height: 40px;
+            margin-right: 8px;
+          }
+        }
+        .business_li_r {
+          font-size: 14px;
+          font-family: PingFang SC, PingFang SC;
+          font-weight: 500;
+          color: #666666;
+          line-height: 16px;
+          .bom_text {
+            font-size: 20px;
+            font-family: DIN Medium, DIN Medium;
+            font-weight: 400;
+            color: #171718;
+            line-height: 23px;
+            margin-top: 8px;
+            span {
+              line-height: 16px;
+              font-size: 14px;
+            }
+          }
+        }
+      }
+      .project {
+        margin-top: 12px;
+        display: flex;
+        .project_table {
+          flex: 1;
+          margin-right: 8px;
+          height: 172px;
+          background: #f2f6ff;
+          box-shadow: 0px 2px 0px 0px rgba(62, 115, 236, 0.2);
+          border-radius: 4px 4px 4px 4px;
+          opacity: 1;
+          padding: 16px;
+        }
+
+        .table_li {
+          display: flex;
+          justify-content: space-between;
+          margin-bottom: 10px;
+          width: 100%;
+          .table_name {
+            position: relative;
+            .table_cur {
+              width: 4px;
+              height: 4px;
+              background: #9fbcff;
+              border-radius: 0px 0px 0px 0px;
+              display: inline-block;
+              position: absolute;
+              left: 0;
+              top: 8px;
+            }
+            font-size: 14px;
+            font-family: PingFang SC, PingFang SC;
+            font-weight: 500;
+            color: #666666;
+            line-height: 20px;
+            padding-left: 12px;
+          }
+          .tabel_value {
+            font-size: 14px;
+            font-family: PingFang SC, PingFang SC;
+            font-weight: 400;
+            color: #171718;
+            line-height: 16px;
+          }
+        }
+        .project_table_r {
+          flex: 1;
+          height: 172px;
+          background: #f2f6ff;
+          box-shadow: 0px 2px 0px 0px rgba(62, 115, 236, 0.2);
+          border-radius: 4px 4px 4px 4px;
+          opacity: 1;
+          padding: 16px;
+        }
+      }
     }
   }
 }
