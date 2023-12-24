8fe556216b743ee00dc665d3bf9863934b1f5e2b
2497976487@qq.com <2497976487@qq.com>
2023-12-24 12:15:54 +0800

    feat: bug修复

--- src/h5/views/home/index.vue
+++ src/h5/views/home/index.vue
@@ -297,7 +297,7 @@ onMounted(() => {
 
 .time-txt {
   margin-top: 10px;
-  font-size: 30px;                                                                                                                                                                                                                                                                            `111111`
+  font-size: 30px;
   line-height: 20px;
   color: #13131366;
 }
