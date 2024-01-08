36da130359fb7507d81a9b20f05bf79e9a64d2d2
1019314017@qq.com <1019314017@qq.com>
2024-01-08 10:23:36 +0800

    fix: bug/1/8/09

--- src/views/Workshop/ImmigrantImplement/DataFill/RelocatePlacement/SelfBuildHouse/Index.vue
+++ src/views/Workshop/ImmigrantImplement/DataFill/RelocatePlacement/SelfBuildHouse/Index.vue
@@ -78,11 +78,68 @@
       :row="currentRow"
       @close="(...event) => close(event, 'fill')"
     />
+    <div style="position: fixed; left: -1000px; width: 210mm; padding: 0 40px 0 40px" id="anztable">
+      <h1 style="font-size: 24px; font-weight: bold; text-align: center">{{
+        baseInfo.houseAreaType === 'homestead' ? '选址确认单' : '选房确认单'
+      }}</h1>
+      <div
+        style="
+          display: flex;
+          margin: 20px 0 20px 0;
+          font-size: 18px;
+          justify-content: space-between;
+        "
+      >
+        <div>
+          {{
+            `${baseInfo.areaCodeText} ${baseInfo.townCodeText} ${baseInfo.villageText} ${baseInfo.name} 户号 ${baseInfo.doorNo} `
+          }}</div
+        >
+
+        <div>{{ data }}</div>
+      </div>
+      <div style="margin: 20px 0 20px 0; font-size: 18px; font-weight: bolder"
+        >已完成宅基地基础建设，特此告知。</div
+      >
+      <ElTable
+        v-if="baseInfo.houseAreaType === 'homestead'"
+        :data="tableData"
+        style="width: 100%"
+        header-cell-class-name="table-headers"
+        cell-class-name="table-cellss"
+        border
+      >
+        <ElTableColumn label="序号" width="80" type="index" align="center" header-align="center" />
+        <ElTableColumn label="区块" prop="settleAddress" align="center" header-align="center">
+          <template #default="{ row }">
+            {{ getSettleAddress(row) }}
+          </template>
+        </ElTableColumn>
+        <ElTableColumn label="类型" prop="houseAreaType" align="center" header-align="center">
+          <template #default="{ row }">
+            {{ row.houseAreaType === 'flat' ? '公寓房' : '宅基地' }}
+          </template>
+        </ElTableColumn>
+        <ElTableColumn label="地块编号" prop="landNo" align="center" header-align="center">
+          <template #default="{ row }">
+            {{ row.landNo }}
+          </template>
+        </ElTableColumn>
+        <ElTableColumn label="备注" align="center" />
+      </ElTable>
+      <div style="display: flex; justify-content: space-between; height: 50px">
+        <div style="line-height: 50px; border: 1px solid black; border-top: 0px; flex: 1"
+          >户主代表或收委托人(签名)：</div
+        ><div style="line-height: 50px; border: 1px solid black; border-top: 0px; flex: 1">
+          联系移民干部(签名)：</div
+        >
+      </div>
+    </div>
   </WorkContentWrap>
 </template>
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue'
-import { ElButton, ElSpace } from 'element-plus'
+import { ElButton, ElSpace, ElTable, ElTableColumn } from 'element-plus'
 import dayjs from 'dayjs'
 import {
   getSelfBuildHouseApi,
@@ -91,7 +148,12 @@ import {
 import { WorkContentWrap } from '@/components/ContentWrap'
 import OnDocumentation from './OnDocumentation.vue' // 引入档案上传组件
 import Fill from './Fill.vue' // 引入填写/查看组件
-
+import { useAppStore } from '@/store/modules/app'
+import { getPlacementPointListApi } from '@/api/systemConfig/placementPoint-service'
+import { resettleArea } from '../../config'
+import { getImmigrantChooseHouseApi } from '@/api/immigrantImplement/siteConfirmation/siteSel-service'
+import { htmlToPdf } from '@/utils/ptf'
+import { debounce } from '@/utils/index'
 interface PropsType {
   doorNo: string
   baseInfo: any
@@ -132,7 +194,14 @@ const close = (params: any[], type: string) => {
  * 打印
  * @param data 当前阶段数据
  */
+let data = ref()
 const onPrint = async (data: any) => {
+  data.value = dayjs(new Date()).format('YYYY年MM月DD日')
+  debounce(() => {
+    // ElMessage.error('待业主提供模板')
+
+    htmlToPdf('#anztable')
+  })
   currentRow.value = data
   await saveSelfBuildHouseForPrint()
 }
@@ -156,9 +225,62 @@ const saveSelfBuildHouseForPrint = async () => {
   await saveSelfBuildHouseApi(params)
   initData()
 }
-
+const appStore = useAppStore()
+let apartmentArea: any = []
+const getSettleAddressList = async () => {
+  const params = {
+    projectId: appStore.getCurrentProjectId,
+    status: 'implementation',
+    type: '2',
+    size: 9999,
+    page: 0
+  }
+  try {
+    const result = await getPlacementPointListApi(params)
+    apartmentArea = result.content
+  } catch {}
+}
+const getSettleAddress = (data: any) => {
+  // 选择了公寓房的安置方式
+  if (data.houseAreaType === 'flat') {
+    let str = ''
+    apartmentArea.map((item: any) => {
+      if (item.id == data.settleAddress) {
+        str = item.name
+      }
+    })
+    return str
+  } else {
+    let str = ''
+    resettleArea.map((item: any) => {
+      if (item.id === data.settleAddress) {
+        str = item.name
+      }
+    })
+    return str
+  }
+}
+let tableData = ref()
+const getList = () => {
+  getImmigrantChooseHouseApi(props.doorNo).then((res) => {
+    const arr: any = []
+    if (res && res?.content.length) {
+      res.content.map((item: any) => {
+        arr.push({
+          ...item,
+          landNoOptions: [],
+          storeroomNoOptions: [],
+          carNoOptions: [],
+          roomNoOptions: []
+        })
+      })
+      tableData.value = arr
+    }
+  })
+}
 onMounted(() => {
   initData()
+  getList()
 })
 </script>
 
@@ -269,6 +391,26 @@ onMounted(() => {
           }
         }
       }
+    }
+  }
+}
+
+#anztable {
+  :deep(.table-headers) {
+    font-size: 12px;
+    font-weight: bold;
+    background: none;
+  }
+
+  .el-table {
+    --el-table-border-color: black;
+    --el-table-border: 1px solid black;
+  }
+
+  :deep(.table-cellss) {
+    .cell {
+      font-size: 10px;
+      background: none;
     }
   }
 }
