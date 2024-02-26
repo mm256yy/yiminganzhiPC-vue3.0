66c6b7475887de03a8b2c0cbb6cfb0182bc7139b
fcx <wy15869011453@163.com>
2024-02-26 11:07:26 +0800

    feat: 合并提交

---
+++ src/h5/components/Expect/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-container">
+    <div class="expect-center">
+      <div>
+        <img style="width: 180px; height: 150px" :src="iconExpectSrc" />
+      </div>
+      <span class="label-title">{{ props.title }}</span>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import iconExpectSrc from '@/h5/assets/imgs/icon_expect.png'
+
+const props = defineProps<{
+  title: string
+}>()
+</script>
+
+<style lang="less" scoped>
+.expect-center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 200px;
+
+  .label-title {
+    font-size: 34px;
+    font-weight: 600;
+    color: #666;
+    margin-top: 40px;
+    font-weight: bold;
+  }
+}
+</style>
