42d9ab3ad8ec0921823a9a09b55dcfaa2339ff6e
1019314017@qq.com <1019314017@qq.com>
2024-04-19 10:34:22 +0800

    fix: 提交备注13

--- src/views/Workshop/ImmigrantImplement/DataFill/components/UserInfo.vue
+++ src/views/Workshop/ImmigrantImplement/DataFill/components/UserInfo.vue
@@ -333,8 +333,8 @@ const fillData = (row, name: any, index?: any) => {
     push({
       name: 'ImmigrantImpDataFill',
       query: {
-        householdId: props.baseInfo.relateIndividualId,
-        doorNo: props.baseInfo.relateIndividualDoorNo,
+        householdId: props.baseInfo.relateIndividualId.split(',')[index],
+        doorNo: props.baseInfo.relateIndividualDoorNo.split(',')[index],
         type: 'IndividualB'
       }
     })
